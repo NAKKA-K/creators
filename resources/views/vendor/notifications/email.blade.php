54f45eb443335d2333c0e5e439bb6b838fb026e3
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-09-10 01:11:54 +0900

    Add send password reset mail

---
+++ resources/views/vendor/notifications/email.blade.php
@@ -0,0 +1,62 @@
+@component('mail::message')
+{{-- Greeting --}}
+@if (! empty($greeting))
+# {{ $greeting }}
+@else
+@if ($level == 'error')
+# @lang('Whoops!')
+@else
+# @lang('いつもご利用ありがとうございます')
+@endif
+@endif
+
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+    switch ($level) {
+        case 'success':
+        case 'error':
+            $color = $level;
+            break;
+        default:
+            $color = 'primary';
+    }
+?>
+@component('mail::button', ['url' => $actionUrl, 'color' => $color])
+{{ $actionText }}
+@endcomponent
+@endisset
+
+{{-- Outro Lines --}}
+@foreach ($outroLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Salutation --}}
+@if (! empty($salutation))
+{{ $salutation }}
+@else
+{{ config('app.name') }}より
+@endif
+
+{{-- Subcopy --}}
+@isset($actionText)
+@component('mail::subcopy')
+@lang(
+    "もし \":actionText\" ボタンがうまく機能しない場合、以下のURLをコピーしてブラウザから直接アクセスしてください。\n".
+    '[:actionURL](:actionURL)',
+    [
+        'actionText' => $actionText,
+        'actionURL' => $actionUrl
+    ]
+)
+@endcomponent
+@endisset
+@endcomponent
