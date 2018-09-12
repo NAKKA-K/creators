5358df39fe5ec91383fa87001abfc5ac5803e164
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-09-12 12:08:06 +0900

    Update title max length

--- database/migrations/2018_09_12_105151_create_events_table.php
+++ database/migrations/2018_09_12_105151_create_events_table.php
@@ -15,7 +15,7 @@ class CreateEventsTable extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name', 127);
             $table->string('description');
             $table->string('readme');
             $table->boolean('published');
