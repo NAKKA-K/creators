58560715e73de34080fc0a2fd2e8845f2112c9a9
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-09-12 22:57:01 +0900

    Add ramdom event seed

---
+++ database/seeds/EventsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Event;
+
+class EventsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('events')->insert([
+            [
+                'name' => 'Test Event',
+                'description' => 'test',
+                'readme' => 'test',
+                'published' => false,
+                'created_at' => time(),
+                'updated_at' => time(),
+            ]
+        ]);
+
+        factory(App\Event::class, 10)->create();
+    }
+}
