c8c5dbbc098a8aa98d207eb4c9086a1feac7f778
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-09-12 12:26:58 +0900

    Update increments

--- database/migrations/2018_09_10_224251_create_roles_table.php
+++ database/migrations/2018_09_10_224251_create_roles_table.php
@@ -14,7 +14,7 @@ class CreateRolesTable extends Migration
     public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->integer('id')->unsigned()->primary();
+            $table->increments('id');
             $table->string('name')->unique();
             $table->timestamps();
         });
