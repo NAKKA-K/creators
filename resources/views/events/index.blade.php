04820771692f7ba9c88cef5e777bccbec96a3ed2
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-09-13 13:06:12 +0900

    Add more info & url

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -4,12 +4,18 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            <a href="{{ route('events.create') }}" class="float-right btn btn-primary">イベント作成</a>
             <h1 class="mt-4 mb-4 mr-2">イベント一覧</h1>
 
             @foreach($events as $event)
                 <div class="card border-secondary mb-2">
                     <div class="card-header">
                         <a href="#"><h4>{{ $event->name }}</h4></a>
+                        <div class="small text-muted">
+                            <a href="#" class="text-secondary">{{ $event->user->name }}</a>
+                            &nbsp;&nbsp;
+                            {{ $event->created_at }}
+                        </div>
                     </div>
                     <div class="card-body">
                         {{ $event->description }}
