46c50b68349a33af7e901d7012f2498cbf751c50
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-09-14 13:27:29 +0900

    Add user to events relation

--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     public function role() {
         return $this->belongsTo('App\Role');
     }
+
+    public function events() {
+        return $this->hasMany('App\Event');
+    }
 }
