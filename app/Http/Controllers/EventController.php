72e8ed20bc2a89f08b3be02c477b442224399952
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-09-16 00:05:57 +0900

    Add event show controller

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -47,7 +47,7 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        //
+        return view('events.show', ['event' => $event]);
     }
 
     /**
