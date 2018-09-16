68dd036fd16d5b433c296826c201888fbf434b39
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-09-16 19:46:09 +0900

    Add relation methods to EventParticipant

--- app/EventParticipant.php
+++ app/EventParticipant.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class EventParticipant extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function event() {
+        return $this->belongsTo('App\Event');
+    }
 }
