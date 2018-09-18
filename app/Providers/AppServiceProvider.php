37c309f29c64f33ce8333fc60e348593ebfd1a11
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-09-18 14:38:58 +0900

    Add errors block directive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,16 @@ class AppServiceProvider extends ServiceProvider
                 $sql = preg_replace("/\?/", $query->bindings[$i], $sql, 1);
             }
             \Log::debug($sql);
+        });
+
+        Blade::directive('errorsBlock', function ($errors){
+            return "<?php
+                echo '<div class=\"invalid-feedback\"><ul>';
+                foreach($errors as \$error){
+                    echo \"<li>\$error</li>\";
+                }
+                echo '</li></div>';
+                ?>";
         });
     }
 
