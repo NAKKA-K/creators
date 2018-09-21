f69aab87fd2408de81933f2ef7e7412b9f19a578
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-09-22 02:19:51 +0900

    Add creative resource

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+   .js('resources/js/creative.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .sass('resources/sass/footer.scss', 'public/css')
+   .sass('resources/sass/creative.scss', 'public/css')
    .sass('resources/sass/user_profile_fonts.scss', 'public/css');
