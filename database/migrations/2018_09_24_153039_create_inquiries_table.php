6f386c9e3a3e2f21e6437796748f325a92a9fad3
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-09-24 22:45:51 +0900

    Add make:model Inquiry -m

---
+++ database/migrations/2018_09_24_153039_create_inquiries_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInquiriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inquiries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('title', 127):
+            $table->string('body', 2047):
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inquiries');
+    }
+}
