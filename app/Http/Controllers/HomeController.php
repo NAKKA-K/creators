16042100be3aab11c883cd9aece44b21f5008af8
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-09-24 15:09:48 +0900

    Add home controllers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,4 +15,24 @@ class HomeController extends Controller
     {
         return view('home.about');
     }
+
+    public function guideline()
+    {
+        return view('home.guideline');
+    }
+
+    public function privacy()
+    {
+        return view('home.privacy');
+    }
+
+    public function help()
+    {
+        return view('home.help');
+    }
+
+    public function termsOfService()
+    {
+        return view('home.terms_of_service');
+    }
 }
