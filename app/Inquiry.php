3a224d8f4d7ffb2a756c9e49d74047a4cef1e908
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-09-25 21:58:57 +0900

    Fix syntax error

--- app/Inquiry.php
+++ app/Inquiry.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Inquiry extends Model
 {
     protected $fillable = [
-        'title', 'body'
-    ]:
+        'title', 'body',
+    ];
 
     public function user(){
         return $this->belongsTo('App\User');
