cc041cf7577f8b23a2a982fe42fa1c9d85673abe
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-09-28 00:35:12 +0900

    Add href of home inquiry

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,7 +4,7 @@
       <li><a href="{{ route('home.terms_of_service') }}">利用規約</a></li>
       <li><a href="{{ route('home.privacy') }}">プライバシー</a></li>
       <li><a href="{{ route('home.help') }}">ヘルプ</a></li>
-      <li><a href="#">お問い合わせ</a></li>
+      <li><a href="{{ route('home.inquiry') }}">お問い合わせ</a></li>
     </ul>
 
     <ul class="service-links">
