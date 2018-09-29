3e3db1d34953b2cebb1176348706a2612111c0fa
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-09-29 18:51:33 +0900

    Update events cards to shared template

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -9,19 +9,7 @@
             </h1>
 
             @foreach($events as $event)
-                <div class="card border-secondary mb-2">
-                    <div class="card-header">
-                        <a href="{{ route('events.show', ['event' => $event]) }}"><h4>{{ $event->name }}</h4></a>
-                        <div class="small text-muted">
-                            <a href="{{ route('users.show', ['user' => $event->user]) }}" class="text-secondary">{{ $event->user->name }}</a>
-                            &nbsp;&nbsp;
-                            {{ $event->created_at }}
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        {{ $event->description }}
-                    </div>
-                </div>
+                @include('shared/event_card', ['event' => $event])
             @endforeach
         </div>
     </div>
