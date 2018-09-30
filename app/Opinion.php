1fd5e474913ec35c1c3bc529e61930e6b8f02246
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-09-30 17:38:06 +0900

    Add relation

--- app/Opinion.php
+++ app/Opinion.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Opinion extends Model
 {
-    //
+    protected $fillable = [
+        'body',
+    ];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
 }
