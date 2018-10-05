3c957e2b71798e7adc0c8254b913b42251237a1e
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-10-05 16:24:21 +0900

    Update url

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::get('login/{provider}/', 'SocialAccountController@redirectToProvider')
 Route::get('login/{provider}/callback/', 'SocialAccountController@handleProviderCallback')
     ->where('provider', '(github|twitter|facebook)');
 
+Route::get('mypage/', 'UserController@myPage');
 Route::resource('events', 'EventController');
 Route::resource('users', 'UserController')->only(['index', 'show']);
 Route::resource('events.event_participants', 'EventParticipantController')->only(['index', 'store', 'destroy']);
