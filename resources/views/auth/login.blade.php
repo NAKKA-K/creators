36c49de798a8c7d4b80a2dcba864b449da803f5a
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-10-05 09:57:42 +0900

    Update display position of login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -58,16 +58,14 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('ログイン') }}
-                                </button>
+                        <div class="form-group row d-flex flex-column align-items-center">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('ログイン') }}
+                            </button>
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('パスワードを忘れましたか?') }}
-                                </a>
-                            </div>
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('パスワードを忘れましたか?') }}
+                            </a>
                         </div>
                     </form>
                 </div>
