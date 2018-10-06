30f3be4f870394dc6508223b6967654c6c063478
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-10-07 00:40:06 +0900

    Move oauth buttons to shared template

---
+++ resources/views/shared/oauth_buttons.blade.php
@@ -0,0 +1,5 @@
+<div class="form-group row justify-content-center">
+    <a href="/login/github" class="page-link text-dark d-inline-block rounded-0 btn-lg ml-3"><span class="fa fa-github text-dark"></span> GitHub</a>
+    <a href="/login/twitter" class="page-link text-dark d-inline-block rounded-0 ml-3 btn-lg"><span class="fa fa-twitter text-info"></span> Twitter</a>
+    <a href="/login/facebook" class="page-link text-dark d-inline-block rounded-0 ml-3 btn-lg"><span class="fa fa-facebook badge-primary"></span> Facebook</a>
+</div>
