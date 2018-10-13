51a17acdabcde21b2af84abe7b37cd32d0a92d97
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-10-14 03:19:00 +0900

    Update local variable to instance variable

--- tests/Feature/EventTest.php
+++ tests/Feature/EventTest.php
@@ -5,17 +5,21 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Auth;
 
 class EventTest extends TestCase
 {
     use RefreshDatabase;
 
+    private $user;
+    private $event;
+
     public function setUp()
     {
         parent::setUp();
-        $user = factory(\App\User::class)->create();
-        $event = factory(\App\Event::class)->create([
-            'user_id' => $user->id
+        $this->user = factory(\App\User::class)->create();
+        $this->event = factory(\App\Event::class)->create([
+            'user_id' => $this->user->id
         ]);
     }
 
@@ -29,14 +33,18 @@ class EventTest extends TestCase
     {
         $this->get('events/create/')
             ->assertRedirect('login/');
+
+        Auth::login($this->user, true);
+        $this->get('events/create/')
+            ->assertStatus(200);
     }
 
     public function testShow()
     {
-        $this->get('events/1')
-            ->assertStatus(200);
-
         $this->get('events/0')
             ->assertStatus(404);
+
+        $this->get('events/1')
+            ->assertStatus(200);
     }
 }
