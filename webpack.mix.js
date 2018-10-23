8450de2113f7c8c6369b7d05e97f50f6b482e833
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-10-24 02:36:35 +0900

    Add react preset & Remove vue preset

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+let mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.react('resources/js/app.js', 'public/js')
    .js('resources/js/creative.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .sass('resources/sass/footer.scss', 'public/css')
